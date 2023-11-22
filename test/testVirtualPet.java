19de9d65af11b0753b703e5416bee67d9e5273b3
ninalui <lui.n@northeastern.edu>
2023-11-22 00:55:27 -0800

    nov21

---
+++ test/testVirtualPet.java
@@ -0,0 +1,89 @@
+/**
+ * JUnit test class for the virtual pet game.
+ */
+public class testVirtualPet {
+
+  /**
+   * Tests the constructor of the virtual pet game and that all values are initialized correctly.
+   */
+
+  /**
+   * Tests the feed method of the virtual pet game.
+   * Hunger need level should increase accordingly with life stage.
+   */
+
+  /**
+   * Tests the play method of the virtual pet game.
+   * Social need level should increase accordingly with life stage.
+   */
+
+  /**
+   * Tests the clean method of the virtual pet game.
+   * Hygiene need level should increase accordingly with life stage.
+   */
+
+  /**
+   * Tests the sleep method of the virtual pet game.
+   * Energy need level should increase accordingly with life stage.
+   */
+
+  /**
+   * Test that the life stage changes correctly.
+   */
+
+  /**
+   * Test that the mood changes to a single low need correctly.
+   */
+
+  /**
+   * Test that the pet becomes happy after its need is met.
+   */
+
+  /**
+   * Test that the pet becomes sad correctly.
+   */
+
+  /**
+   * Test that the pet becomes sick correctly.
+   */
+
+  /**
+   * Test that the pet becomes healthy after its needs are met.
+   */
+
+  /**
+   * Test that the pet's need levels decrease as scheduled correctly.
+   */
+
+  /**
+   * Test that the pet's need levels decrease corresponding to the life stage correctly.
+   */
+
+  /**
+   * Test that changing life stage changes need increases correctly.
+   */
+
+  /**
+   * Test that changing life stage changes need decreases correctly.
+   */
+
+  /**
+   * Test that the pet dies when its health reaches 0.
+   */
+
+  /**
+   * Test that the game ends when the pet dies.
+   */
+
+  /**
+   * Test that the player cannot interact with the pet after it dies.
+   */
+
+  /**
+   * Test that the player cannot perform an action when the pet's need is already full.
+   */
+
+  /**
+   *
+   */
+}
